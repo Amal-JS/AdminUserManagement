4c6e6d0d7540f44216bc7c7de2d771a4c3787ab5
amaljs <amaljs121@gmail.com>
2023-12-13 13:21:22 +0530

    intialized a react project with typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import {Button} from "@nextui-org/react";
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <Button color="primary" variant="flat">
+        Flat
+      </Button> 
+       </div>
+    </>
+  )
+}
+
+export default App
