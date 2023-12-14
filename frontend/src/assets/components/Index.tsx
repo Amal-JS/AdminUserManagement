a73b3832ad8cd77e925b61a0e444003be2799510
amaljs <amaljs121@gmail.com>
2023-12-14 12:33:17 +0530

    routes added

--- frontend/src/assets/components/Index.tsx
+++ frontend/src/assets/components/Index.tsx
@@ -1,6 +1,8 @@
 import {BrowserRouter as Router , Routes , Route } from 'react-router-dom'
 import { AdminHome } from './AdminHome'
+import { AdminLogin } from './AdminLogin'
 import { UserHome } from './UserHome'
+import { UserLogin } from './UserLogin'
 export const Index =()=>{
     return (
         <>
@@ -8,6 +10,8 @@ export const Index =()=>{
             <Routes>
                 <Route path='/' element={<UserHome/>}></Route>
                 <Route path='/adminHome' element={<AdminHome/>}></Route>
+                <Route path='/userLogin' element={<UserLogin/>}></Route>
+                <Route path='/adminLogin' element={<AdminLogin/>}></Route>
             </Routes>
         </Router>
         </>
