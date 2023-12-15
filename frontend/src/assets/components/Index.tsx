71e186ebd3c953280c96634452185a38bd861a8b
amaljs <amaljs121@gmail.com>
2023-12-15 21:01:27 +0530

    added user edit component

--- frontend/src/assets/components/Index.tsx
+++ frontend/src/assets/components/Index.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router , Routes , Route } from 'react-router-dom'
 import { AdminHome } from './AdminHome'
 import { AdminLogin } from './AdminLogin'
+import { UserEdit } from './UserEdit'
 import { UserHome } from './UserHome'
 import { UserLogin } from './UserLogin'
 import { UserSignUp } from './UserSignUp'
@@ -10,7 +11,10 @@ export const Index =()=>{
         <Router>
             <Routes>
                 <Route path='/' element={<UserHome/>}></Route>
-                <Route path='/adminHome' element={<AdminHome/>}></Route>
+                <Route path='admin'>
+                    <Route index element={<AdminHome />}/>
+                    <Route path="edit" element={<UserEdit/>}/>
+                </Route>
                 <Route path='/userLogin' element={<UserLogin/>}></Route>
                 <Route path='/adminLogin' element={<AdminLogin/>}></Route>
                 <Route path='/userSignUp' element={<UserSignUp/>}></Route>
