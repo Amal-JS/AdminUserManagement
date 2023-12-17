a45b2a36fae2c3f06413e6e378f7ad922fe70d35
amaljs <amaljs121@gmail.com>
2023-12-17 18:13:39 +0530

    profile picture field added

--- backend/data/models.py
+++ backend/data/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.hashers import make_password
 
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=10)
+    image = models.ImageField(upload_to='profile_pictures/',default='',null=True,blank=True)
 
     def save(self, *args, **kwargs):
         # Hash the password before saving the user
